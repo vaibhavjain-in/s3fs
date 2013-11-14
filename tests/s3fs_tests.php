546d429f148f3eea1453a88268c8276ae20f9a53
Robert Rollins <rrollins@caltech.edu>
2013-11-13 16:13:20 -0800

    More cleanup for full project approval.

--- tests/s3fs_tests.php
+++ tests/s3fs_tests.php
@@ -1,4 +1,4 @@
-#!/opt/local/bin/php
+#!/usr/bin/env php
 <?php
 
 /**
@@ -39,6 +39,7 @@ function _s3fs_main() {
 
 function _s3fs_upload_test($filename) {
   $uploaded_uri = "s3://_s3fs_test/test_file.jpg";
+  $uploaded_uri2 = "s3://_s3fs_test/test_file2.jpg";
   
   _s3fs_assert(file_stream_wrapper_valid_scheme('s3'), __LINE__, '"s3" should be a valid stream wrapper scheme.');
   _s3fs_assert(file_stream_wrapper_get_class('s3') == 'S3StreamWrapper', __LINE__, 'URIs with scheme "s3" should be handled by S3StreamWrapper.');
@@ -73,10 +74,12 @@ function _s3fs_upload_test($filename) {
   _s3fs_assert(file_valid_uri($s3_file->uri), __LINE__, 'The URI to the file we just uploaded should be valid.');
   
   _s3fs_test_print('Exercising the dir_*() functions.');
+  // Add a second file to the folder.
+  $s3_file2 = file_save_data($file_contents, $uploaded_uri2, FILE_EXISTS_REPLACE);
   $files = file_scan_directory('s3://_s3fs_test', '#.*#');
   _s3fs_assert(isset($files[$uploaded_uri]), __LINE__, 'The file we uploaded is not being reported as an element of the testing directory.');
   $file_count = count($files);
-  _s3fs_assert($file_count == 1, __LINE__, "There should be exactly 1 file in the s3://_s3fs_test directory. Instead, there are $file_count.");
+  _s3fs_assert($file_count == 2, __LINE__, "There should be exactly 2 files in the s3://_s3fs_test directory. Instead, there are $file_count.");
   
   _s3fs_test_print('Exercising getExternalUri().');
   $url = file_create_url($uploaded_uri);
@@ -86,18 +89,19 @@ function _s3fs_upload_test($filename) {
   _s3fs_assert(strstr($url, '/s3/files/styles') !== FALSE, __LINE__, 'A private filesystem path was not created.');
   
   _s3fs_test_print('Exercising unlink() by moving a file.');
-  $moved_uri = 's3://_s3fs_test/test_file2.jpg';
+  $moved_uri = 's3://_s3fs_test/test_file-moved.jpg';
   $ret = file_move($s3_file, $moved_uri, FILE_EXISTS_REPLACE);
-  _s3fs_assert($ret, __LINE__, 'Failed to move the file to test_file2.jpg');
+  _s3fs_assert($ret, __LINE__, 'Failed to move the file to test_file-moved.jpg');
   $ret = file_exists('s3://_s3fs_test/test_file.jpg');
-  _s3fs_assert($ret == FALSE, __LINE__, 'The old file should not longer exist.');
+  _s3fs_assert($ret == FALSE, __LINE__, 'The old file should no longer exist.');
   
   _s3fs_test_print('Exercising rename() by moving the file back to the original uri.');
   _s3fs_assert(rename($moved_uri, $uploaded_uri), __LINE__, 'Call to rename() failed.');
   
   _s3fs_test_print('Exercising rmdir().');
   _s3fs_assert(!drupal_rmdir('s3://_s3fs_test'), __LINE__, 'The rmdir() call should have failed, because s3://_s3fs_test is not empty.');
-  _s3fs_assert(drupal_unlink($uploaded_uri), __LINE__, 'The delete call for the moved file failed.');
+  _s3fs_assert(drupal_unlink($uploaded_uri), __LINE__, 'The delete call for the first file failed.');
+  _s3fs_assert(drupal_unlink($uploaded_uri2), __LINE__, 'The delete call for the second file failed.');
   _s3fs_assert(drupal_rmdir('s3://_s3fs_test'), __LINE__, 'The rmdir() call should have succeeded, because s3://_s3fs_test is empty now.');
   _s3fs_assert(!is_dir('s3://_s3fs_test'), __LINE__, 's3://_s3fs_test is gone, and should not be considered a directory any more.');
   
