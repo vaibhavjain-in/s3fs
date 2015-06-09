b6abb5e17349a8e7495ff2d79b385bf3881eaeb6
Robert Rollins <rrollins@caltech.edu>
2015-06-09 11:31:30 -0700

    Issue 2503007: Added support for Signature v4.

--- s3fs.api.php
+++ s3fs.api.php
@@ -49,7 +49,7 @@ function hook_s3fs_url_settings_alter(&$url_settings, $s3_file_path) {
  *   Associative array of upload settings
  * @see http://docs.aws.amazon.com/aws-sdk-php/latest/class-Aws.S3.S3Client.html#_putObject
 */
-function hook_s3fs_upload_params_alter(&$upload_params){
+function hook_s3fs_upload_params_alter(&$upload_params) {
   if (strpos($upload_params['Key'], 'private/') !== FALSE){
     $upload_params['ACL'] = 'private';
   }
